e9bf211b9a534523e1ec91def074e72d3f736a14
Witold <witold.wojnarowicz@gmail.com>
2023-03-19 22:18:02 +0100

    Initial commit

---
+++ src/test/java/com/wojnarowicz/springcloud/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.wojnarowicz.springcloud.configserver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertEquals(4, 2 + 2);
+	}
+
+}
